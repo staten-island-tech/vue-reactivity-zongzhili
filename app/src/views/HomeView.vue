968a4dae0d3f88f64f41af69de31235709fcae51
zongzhili <zongzhil@nycstudents.net>
2025-01-31 14:01:44 -0500

    aa

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,8 +2,15 @@
   <div>
     <h1>Fresh Pizza</h1>
     <div class="pizza-grid">
-      <div v-for="pizza in pizzas">
-        <li>{{ pizza.name }} - ${{ pizza.price }}</li>
+      <div v-for="pizza in pizzas" :key="pizza.name">
+        <ul>
+          {{
+            pizza.name
+          }}
+          - ${{
+            pizza.price
+          }}
+        </ul>
         <img :src="pizza.image" alt="Pizza image" class="pizza-image" />
       </div>
     </div>
@@ -11,79 +18,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const pizzas = ref([
-  {
-    name: 'Margherita',
-    price: 8.99,
-    image:
-      'https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067',
-  },
-  {
-    name: 'Pepperoni',
-    price: 10.99,
-    image:
-      'https://www.sipandfeast.com/wp-content/uploads/2022/07/spicy-pepperoni-pizza-recipe-snippet.jpg',
-  },
-  {
-    name: 'Hawaiian',
-    price: 11.49,
-    image:
-      'https://hips.hearstapps.com/hmg-prod/images/hawaiian-pizza-index-65f4641de4b08.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*',
-  },
-  {
-    name: 'Chicago Deep Dish',
-    price: 9.99,
-    image:
-      'https://res.cloudinary.com/hksqkdlah/image/upload/SFS_chicago_deep_dish_pizza-16_cehf9d.jpg',
-  },
-  {
-    name: 'BBQ Chicken',
-    price: 12.99,
-    image: 'https://breadboozebacon.com/wp-content/uploads/2023/05/BBQ-Chicken-Pizza-SQUARE.jpg',
-  },
-  {
-    name: 'Neapolitan',
-    price: 9.49,
-    image: 'https://www.fornobravo.com/wp-content/uploads/2023/07/Margherita-Pizza-rotated.jpeg',
-  },
-  {
-    name: 'Meat Lovers',
-    price: 13.49,
-    image:
-      'https://www.thespruceeats.com/thmb/xuxwh4RIGcZMgaJE8u3SueM0SoA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/aqIMG_4568fhor-0b89dc5c8c494ee9828ed29805791c5a.jpg',
-  },
-  {
-    name: 'Four Cheese',
-    price: 11.99,
-    image:
-      'https://us.gozney.com/cdn/shop/articles/Four_Cheese_Pizza_Feng_Chen_-_Large1.jpg?v=1696535082',
-  },
-  {
-    name: 'Seafood',
-    price: 14.49,
-    image:
-      'https://images.squarespace-cdn.com/content/v1/5e3b571c57c3897ac7789b45/1580959991308-Y71RDBC7B8YFV53A9B2K/2020_Angelakis_MarinaraMix_Pizza_Website-1.jpg',
-  },
-  {
-    name: 'Buffalo Chicken',
-    price: 12.49,
-    image:
-      'https://www.thespruceeats.com/thmb/bFLeGj1LnKO9ow7XIcddeRovVQU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-buffalo-chicken-pizza-8580296-hero-01-45dad919df034cc3bf73d774250e44d2.jpg',
-  },
-  {
-    name: 'Pesto Chicken',
-    price: 11.79,
-    image:
-      'https://i2.wp.com/lifemadesimplebakes.com/wp-content/uploads/2019/09/chicken-pesto-pizza-square-1200.jpg',
-  },
-  {
-    name: 'Greek',
-    price: 10.49,
-    image:
-      'https://www.healthyseasonalrecipes.com/wp-content/uploads/2019/12/greek-pizza-21-001.jpg',
-  },
-])
+import { reactive } from 'vue'
+import { pizzas } from '@/assets/array.js'
+
+const pizzasArray = reactive(pizzas)
 </script>
 
 <style scoped>
@@ -101,8 +39,14 @@ body,
   align-items: center;
   justify-content: space-evenly;
   justify-content: center;
+  gap: 10px;
 }
 .pizza-image {
-  width: 200px;
+  width: 400px;
+  height: 400px;
+  object-fit: cover;
+}
+h1 {
+  text-align: center;
 }
 </style>
